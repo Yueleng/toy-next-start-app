a1f44b8a520679cd910e7357d752423435d86661
Alan <wangyueleng@gmail.com>
2025-12-27 12:30:05 -0500

    Add protected home page with authentication check
    Introduces a new page that checks for user authentication using the auth library. If the user is not authenticated, they are redirected to the sign-in page; otherwise, their email is displayed along with a sign-out option.

---
+++ auth-nextjs-tutorial-master/src/app/page.tsx
@@ -0,0 +1,23 @@
+import { SignOut } from "@/components/sign-out";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
+
+const Page = async () => {
+  // Check if the user is authenticated
+  const session = await auth();
+  if (!session) redirect("/sign-in");
+
+  // Render the page after authentication passed
+  return (
+    <>
+      <div className="bg-gray-100 rounded-lg p-4 text-center mb-6">
+        <p className="text-gray-600">Signed in as:</p>
+        <p className="font-medium">{session.user?.email}</p>
+      </div>
+
+      <SignOut />
+    </>
+  );
+};
+
+export default Page;
